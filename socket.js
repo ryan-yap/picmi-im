3404f846cd112e709152583ba1ce629c1cfcd61a
Ryan <ykshiang@gmail.com>
2015-03-26 11:35:35 -0700

    separate socket server

--- socket.js
+++ socket.js
@@ -1,11 +1,25 @@
+var redis = require('redis'),
+    client = redis.createClient()
+
+var proximity = require('geo-proximity').initialize(client)
+
 var app = require('http').createServer()
 var io = require('socket.io')(app);
 
 app.listen(8080);
 
-io.on('connection', function (socket) {
-  console.log("New Connection")
-  socket.on('ping', function (data) {
-    console.log(data);
+io.on("connection", function(socket){
+    console.log("new connection")
+    socket.on("ping", function(data) {
+      info = data.split(":")
+      proximity.addLocation(parseFloat(info[1]), parseFloat(info[2]), info[0], function(err, reply){
+        if(err) console.error(err)
+        else console.log("added " + reply + " location:" + info)
+        return
+      })
+    });
+
+    socket.on("disconnect", function() {
+      return
+    });
   });
-});
