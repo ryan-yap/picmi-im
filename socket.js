936c6d0e1928120b1c566aeac9a752a09b28789d
Ryan <ykshiang@gmail.com>
2015-04-01 16:50:09 -0700

    added notification

--- socket.js
+++ socket.js
@@ -1,8 +1,6 @@
 var redis = require('redis'),
     client = redis.createClient()
 
-var proximity = require('geo-proximity').initialize(client)
-
 var app = require('http').createServer()
 var io = require('socket.io')(app);
 var count = 0
@@ -11,14 +9,36 @@ app.listen(8080);
 
 io.on("connection", function(socket){
     connection_made++;
-    socket.on("ping", function(data) {
-      info = data.split(":")
-      proximity.addLocation(parseFloat(info[1]), parseFloat(info[2]), info[0], function(err, reply){
-        if(err) console.error(err)
-        else console.log("added " + reply + " location:" + info)
-        return
-      })
+    console.log("New connection")
+    socket.on("setID", function(data) {
+      var socket_id = socket.id;
+      client.set(data, socket_id, function(err) {
+        if (err) throw err;
+      });
+      console.log(data)
     });
+
+    socket.on("send", function(data){
+      info = data.split(":");
+      client.get(info[0], function (err, result) {
+          console.log(result)
+          io.sockets.connected[result].emit("receive", info[1]);
+          console.log(data)
+      });
+    })
+
+    socket.on("driverrequest", function(data){
+      info = data.split(":");
+      var driver_id = info[0]
+      var message = info[1]
+      var distance = info[2]
+      var longitude = info[3]
+      var latitude = info[4]
+      var image_url = info[5]
+      client.get(info[0], function (err, result) {
+          io.sockets.connected[result].emit("photorequest", data);
+      });
+    })
 
     socket.on("test", function(data) {
       if (data == "test"){
