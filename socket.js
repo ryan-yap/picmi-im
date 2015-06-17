96d9e3f2a632e72628d247ed3c6e58210b31db30
ryan-yap <ykshiang@gmail.com>
2015-06-17 22:50:37 +0000

    working for version 1.1.2

--- socket.js
+++ socket.js
@@ -1,8 +1,8 @@
-var dispatch_db = require('mongoskin').db('mongodb://52.8.188.79:27017/Dispatch');
+var dispatch_db = require('mongoskin').db('mongodb://54.153.62.38:27017/Dispatch');
 var ObjectID = require('mongoskin').ObjectID
 var Handler = require('./handler') 
 var redis = require('redis'),
-client = redis.createClient(6379, '52.8.106.163', {})
+client = redis.createClient(6379, '54.67.18.228', {})
 var proximity = require('geo-proximity').initialize(client)
 var app = require('http').createServer()
 var io = require('socket.io')(app);
